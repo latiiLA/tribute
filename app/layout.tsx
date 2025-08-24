410b268071f93d6e9d060517ee1c081962c3eccd
Lata <amelatbay@gmail.com>
2025-08-24 18:41:26 +0300

    padding added

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="px-10">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
