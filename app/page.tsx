b501daac4929830ea32c75fa53fa3104e88e7f69
Lata <amelatbay@gmail.com>
2025-08-24 18:42:19 +0300

    home section added

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+"use client";
+
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div id="home-section" className="mx-3, sm:mx-5, md:md-6">
+      <div>
+        <div>Home</div>
+      </div>
+    </div>
+  );
 }
